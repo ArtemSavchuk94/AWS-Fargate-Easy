dc92a04d4b47e3c2bdbf1a41e33ae43ed93e415c
artemsavchuk94 <pro100artemqabin@gmail.com>
2022-08-10 12:49:34 +0300

    add autoscale

--- lib/dashboards-stack.ts
+++ lib/dashboards-stack.ts
@@ -3,6 +3,8 @@ import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as applicationautoscaling from 'aws-cdk-lib/aws-applicationautoscaling';
+import * as loadBalancedFargateService from 'aws-cdk-lib/aws-ecs-patterns';
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -72,6 +74,69 @@ const listener = lb.addListener(`HTTPListener`, {
 
 
 
+
+const scalableTarget = service.autoScaleTaskCount({
+  minCapacity: 1,
+  maxCapacity: 2,
+});
+
+scalableTarget.scaleOnCpuUtilization('CpuScaling', {
+  targetUtilizationPercent: 50,
+});
+
+scalableTarget.scaleOnMemoryUtilization('MemoryScaling', {
+  targetUtilizationPercent: 50,
+});
+
+
+
+
+//const scaling = service.autoScaleTaskCount({ maxCapacity: 1 });
+//scaling.scaleOnSchedule('StartVectorizationTask', {
+  //schedule: Schedule.cron({ minute: '10' }),
+  //maxCapacity: 1,
+  //minCapacity: 1,
+//});
+//scaling.scaleOnSchedule('StopVectorizationTask', {
+  //schedule: Schedule.cron({ minute: '20' }),
+  //maxCapacity: 0,
+  //minCapacity: 0,
+//});
+
+
+
+  //const autoScale = service.autoScaleTaskCount({
+  //minCapacity: 1,
+  //maxCapacity: 2,
+//});
+
+//autoScale.scaleOnCpuUtilization("CPUAutoscaling", {
+  //targetUtilizationPercent: 50,
+  //scaleInCooldown: cdk.Duration.seconds(30),
+  //scaleOutCooldown: cdk.Duration.seconds(30),
+//});
+
+
+
+ //const scaling = service.service.autoScaleTaskCount({ maxCapacity: 3, minCapacity: 1 });
+//scaling.scaleOnCpuUtilization('autoscale_cpu', {
+  //targetUtilizationPercent: 50,
+  //scaleInCooldown: Duration.minutes(2),
+  //scaleOutCooldown: Duration.seconds(30)
+//});
+
+
+//const springbootAutoScaling = exampleApp.service.autoScaleTaskCount({
+  //maxCapacity: 4,
+  //minCapacity: 2
+//})
+
+//springbootAutoScaling.scaleOnCpuUtilization('cpu-autoscaling', {
+  //targetUtilizationPercent: 45,
+ // policyName: "cpu-autoscaling-policy",
+  //scaleInCooldown: cdk.Duration.seconds(30),
+ // scaleOutCooldown: cdk.Duration.seconds(30)
+//})
 
 
 
